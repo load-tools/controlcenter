c5980bf982a6be21b5f5fb7aa0b77b38bdfa83f3
fomars <fomars@yandex-team.ru>
2018-12-27 19:58:37 +0300

    tee stderr log

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,4 +4,4 @@ if [ -z "$APPLICATION_SETTINGS" ];
     then echo "Environment variable APPLICATION_SETTINGS should be set. Exiting.";
     exit 1;
 fi
-exec unbuffer uwsgi --ini uwsgi.ini "$@" | tee uwsgi.log
+exec unbuffer uwsgi --ini uwsgi.ini "$@" |& tee uwsgi.log
